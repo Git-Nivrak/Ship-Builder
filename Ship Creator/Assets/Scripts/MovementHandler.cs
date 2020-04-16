244d6a55bcf27bcde264fa6157d00b4166e010e2
Git-Nivrak <59925169+Git-Nivrak@users.noreply.github.com>
2020-04-16 17:50:07 +0300

    Update MovementHandler.cs

--- Ship Creator/Assets/Scripts/MovementHandler.cs
+++ Ship Creator/Assets/Scripts/MovementHandler.cs
@@ -266,27 +266,4 @@ public class MovementHandler : MonoBehaviour
 
     }
 
-
-
-    
-
-    public Vector3 GetWorldPositionOnPlane(Vector3 screenPosition, float z)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        Plane xy = new Plane(Vector3.up, new Vector3(0, 0, z));
-        float distance;
-        xy.Raycast(ray, out distance);
-        return ray.GetPoint(distance);
-    }
-
-    public Vector3 GetWorldPositionOnPlaneY(Vector3 screenPosition, float z)
-    {
-        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
-        Plane xy = new Plane(Vector3.forward, new Vector3(0, 0, z));
-        float distance;
-        xy.Raycast(ray, out distance);
-        return ray.GetPoint(distance);
-    }
-
-
 }
